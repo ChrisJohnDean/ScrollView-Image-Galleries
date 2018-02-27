0840e63cd9cfe6c12fbfddcf9e105cc1e6ecb44e
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-26 18:13:03 -0800

    Initial Commit

---
+++ ScrollView Image Galleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView Image Galleries
+//
+//  Created by Chris Dean on 2018-02-26.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView Image Galleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView Image Galleries
+//
+//  Created by Chris Dean on 2018-02-26.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
